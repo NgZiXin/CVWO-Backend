a50e40571178ddc610d60f2f48e7d46aa97ed211
NgZiXin <ngzixin02@gmail.com>
2024-01-08 00:23:22 +0800

    Converted database to Postgres
    sqlite3 to postgres db for easy deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001" # Client's domain
+    origins "http://localhost:3001" # Client's domain (development & production)
 
     resource "*",
       headers: :any,
