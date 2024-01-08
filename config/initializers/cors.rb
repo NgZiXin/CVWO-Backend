fe8648bbc260ec6a8c17f86e1a3996cd1214b476
NgZiXin <ngzixin02@gmail.com>
2024-01-08 22:38:23 +0800

    added localhost3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001","https://cvwo-frontend.vercel.app" # Client's domain (development & production)
+    origins "http://localhost:3000","http://localhost:3001","https://cvwo-frontend.vercel.app" # Client's domain (development & production)
 
     resource "*",
       headers: :any,
